44a473175ac09dffe7054e8d6796ce69a62d0315
dvaJf <dvakgj@gmail.com>
2026-04-16 22:05:55 +0300

    email-name

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer,String
 from src.database import Base
 from fastapi_users.db import SQLAlchemyBaseUserTable
 
 class User(SQLAlchemyBaseUserTable[int], Base):
+    email = Column(String)
     id = Column(Integer, primary_key=True, autoincrement=True)
     score = Column(Integer, default=0)
     race_results = relationship("RaceResult", back_populates="user")
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,10 +1,13 @@
 from fastapi_users.schemas import BaseUser, BaseUserCreate, BaseUserUpdate
 
 class UserRead(BaseUser[int]):
+    email: str
     score: int
 
 class UserCreate(BaseUserCreate):
+    email: str
     score: int = 0
 
 class UserUpdate(BaseUserUpdate):
+    email: str | None = None
     score: int | None = None
